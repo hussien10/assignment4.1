3e986ea4582e8c20d022b184d9ffdd2421555d24
hussien10 <32900347+hussien10@users.noreply.github.com>
2021-08-20 05:13:39 +0200

    add addphotos button and change some styles

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 include("./shared/header.php")
 ?>
-<section class="p-5">
+<section class="p-5 text-center">
     <div class="container">
+    <h1 class="text-center mb-5 text-capitalize text-info">our gallery</h1>
     <div class="row">
         <?php
         $images = scandir("./uploads/");
@@ -23,6 +24,7 @@ include("./shared/header.php")
         }
         ?>
     </div>
+    <a class="btn btn-success mt-5" href="upload.php">add Photos</a>
     </div>
 </section>
 <?php
--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
     include("./shared/header.php")
 ?>
 
-<form action="handle-upload.php" class="w-50 mx-auto mt-5 p-5 border" method="POST" enctype="multipart/form-data">
+<form action="handle-upload.php" class="w-50 mx-auto mt-5 p-5 border text-center" method="POST" enctype="multipart/form-data">
 <h1 class="text-center text-black-50 mb-4 text-capitalize">upload images</h1>
     <input class="form-control my-3" type="text" name="imageName" placeholder="image name">
     <input class="form-control my-3" type="file" name="image">
